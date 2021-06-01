ff83f80fb691784e56c0b47650e2911ef71bbb09
Jose Hurtarte <JJHH121106@gmail.com>
2021-06-01 15:49:16 -0600

    Updated dependencies

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Scene(){
+
+    return (
+        <div>
+            El pepe
+        </div>
+
+        
+    );
+}
+
+export default Scene;
--- src/index.js
+++ src/index.js
@@ -3,4 +3,11 @@ import "./style.scss"
 import "bootstrap/dist/js/bootstrap"
 import React from 'react';
 import ReactDom from 'react-dom';
+import App from './App';
 
+ReactDom.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById("root")
+);
