f6a4d15f5e557444108d4134a42fff6337552795
Jose Hurtarte <JJHH121106@gmail.com>
2021-06-04 03:44:51 -0600

    Percentages finish

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,6 +1,8 @@
 import React from "react";
 //para componente 2
 import Retrato from "../../assets/selfie.jpg";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome' ;
+import {faGithub, faLinkedin, faInstagram} from '@fortawesome/free-brands-svg-icons';
 function NavBar() {
   return (
     <div>
@@ -65,7 +67,7 @@ function NavBar() {
                   className="btn btn-outline-secondary btn-lg btn-default"
                   type="button"
                 >
-                  <i className="fa fa-google-plus fa-fw"></i>
+                  <FontAwesomeIcon icon={faLinkedin}/>
                   <span className="network-name">&nbsp; LinkedIn</span>
                 </button>
               </li>
@@ -75,7 +77,7 @@ function NavBar() {
                   className="btn btn-outline-secondary btn-lg btn-default"
                   type="button"
                 >
-                  <i className="fa fa-twitter fa-fw"></i>
+                  <FontAwesomeIcon icon={faInstagram}/>
                   <span className="network-name">&nbsp;Instagram</span>
                 </button>
               </li>
@@ -85,7 +87,7 @@ function NavBar() {
                   className="btn btn-outline-secondary btn-lg btn-default"
                   type="button"
                 >
-                  <i className="fa fa-github fa-fw"></i>
+                  <FontAwesomeIcon icon={faGithub}/>
                   <span className="network-name">&nbsp;github</span>
                 </button>
               </li>
@@ -100,74 +102,162 @@ function NavBar() {
         <div className="row texto-about">
           <div className="col-lg-8  mx-auto">
             <h2 className="display-5">
-              <b>Acerca de</b>
+              <b>Logros y Habilidades</b>
             </h2>
           </div>
         </div>
         <div className="row">
           <div className="d-flex align-items-center col mx-auto list-group">
-          <a href="#" className="list-group-item list-group-item-action">
-    <div className="d-flex w-100 justify-content-between">
-      <h5 className="mb-1">Galardon académico de la Riva</h5>
-      <small className="text-muted">2018</small>
-    </div>
-    <p className="mb-1">Premio a los mejores promedios de la promoción de colegio.</p>
-    <small className="text-muted">Obtenido mientras estudié Bachillerato en Computación</small>
-  </a>
+            <a href="#" className="list-group-item list-group-item-action">
+              <div className="d-flex w-100 justify-content-between">
+                <h5 className="mb-1">Galardon académico de la Riva</h5>
+                <small className="text-muted">2018</small>
+              </div>
+              <p className="mb-1">
+                Premio a los mejores promedios de la promoción de colegio.
+              </p>
+              <small className="text-muted">
+                Obtenido mientras estudié Bachillerato en Computación
+              </small>
+            </a>
 
-  <a href="#" className="list-group-item list-group-item-action">
-    <div className="d-flex w-100 justify-content-between">
-      <h5 className="mb-1">Ingeniería en Ciencias de la Computación</h5>
-      <small className="text-muted">2019-2023</small>
-    </div>
-    <p className="mb-1">Universidad del Valle de Guatemala</p>
-    <small className="text-muted">Actualmente estudiando y especializandome en seguridad informática</small>
-  </a>
+            <a href="#" className="list-group-item list-group-item-action">
+              <div className="d-flex w-100 justify-content-between">
+                <h5 className="mb-1">
+                  Ingeniería en Ciencias de la Computación
+                </h5>
+                <small className="text-muted">2019-2023</small>
+              </div>
+              <p className="mb-1">Universidad del Valle de Guatemala</p>
+              <small className="text-muted">
+                Actualmente estudiando y especializandome en seguridad
+                informática
+              </small>
+            </a>
 
-  <a href="#" className="list-group-item list-group-item-action">
-    <div className="d-flex w-100 justify-content-between">
-      <h5 className="mb-1">Beca Fundación Educación</h5>
-      <small className="text-muted">2020</small>
-    </div>
-    <p className="mb-1">Dada por la organización suiza Fundación educación </p>
-    <small className="text-muted">Beca completa dada a alumnos destacados en carreras tecnológicas</small>
-  </a>
+            <a href="#" className="list-group-item list-group-item-action">
+              <div className="d-flex w-100 justify-content-between">
+                <h5 className="mb-1">Beca Fundación Educación</h5>
+                <small className="text-muted">2020</small>
+              </div>
+              <p className="mb-1">
+                Dada por la organización suiza Fundación educación{" "}
+              </p>
+              <small className="text-muted">
+                Beca completa dada a alumnos destacados en carreras tecnológicas
+              </small>
+            </a>
 
-  <a href="#" className="list-group-item list-group-item-action">
-    <div className="d-flex w-100 justify-content-between">
-      <h5 className="mb-1">Distinción Académica de Estudiante Distinguido</h5>
-      <small className="text-muted">2020</small>
-    </div>
-    <p className="mb-1">Universidad del Valle de Guatemala</p>
-    <small className="text-muted">Obtenida por promedio superior a 85 en el año 2019</small>
-  </a>
+            <a href="#" className="list-group-item list-group-item-action">
+              <div className="d-flex w-100 justify-content-between">
+                <h5 className="mb-1">
+                  Distinción Académica de Estudiante Distinguido
+                </h5>
+                <small className="text-muted">2020</small>
+              </div>
+              <p className="mb-1">Universidad del Valle de Guatemala</p>
+              <small className="text-muted">
+                Obtenida por promedio superior a 85 en el año 2019
+              </small>
+            </a>
 
-  <a href="#" className="list-group-item list-group-item-action">
-    <div className="d-flex w-100 justify-content-between">
-      <h5 className="mb-1">Auxiliar Algoritmos y Programación Básica</h5>
-      <small className="text-muted">2021</small>
-    </div>
-    <p className="mb-1">Universidad del Valle de Guatemala</p>
-    <small className="text-muted">Profesor Auxiliar presencial del curso introductorio de programación</small>
-  </a>
+            <a href="#" className="list-group-item list-group-item-action">
+              <div className="d-flex w-100 justify-content-between">
+                <h5 className="mb-1">
+                  Auxiliar Algoritmos y Programación Básica
+                </h5>
+                <small className="text-muted">2021</small>
+              </div>
+              <p className="mb-1">Universidad del Valle de Guatemala</p>
+              <small className="text-muted">
+                Profesor Auxiliar presencial del curso introductorio de
+                programación
+              </small>
+            </a>
 
-  <a href="#" className="list-group-item list-group-item-action">
-    <div className="d-flex w-100 justify-content-between">
-      <h5 className="mb-1">Distinción Académica Honor al mérito académico</h5>
-      <small className="text-muted">2021</small>
-    </div>
-    <p className="mb-1">Universidad del Valle de Guatemala</p>
-    <small className="text-muted">Obtenida por promedio superior a 90 en el año 2020</small>
-  </a>
-
-  
+            <a href="#" className="list-group-item list-group-item-action">
+              <div className="d-flex w-100 justify-content-between">
+                <h5 className="mb-1">
+                  Distinción Académica Honor al mérito académico
+                </h5>
+                <small className="text-muted">2021</small>
+              </div>
+              <p className="mb-1">Universidad del Valle de Guatemala</p>
+              <small className="text-muted">
+                Obtenida por promedio superior a 90 en el año 2020
+              </small>
+            </a>
           </div>
-          <div className="d-flex align-items-center col mx-auto">
-            <a
-              href="#"
-              className="list-group-item list-group-item-action list-group-item-warning"
-            >
-              A simple warning list group item
+          <div className="d-flex align-items-center col mx-auto list-group">
+            <a href="#" className="list-group-item list-group-item-action">
+              <p className="mb-1">Python:</p>
+              
+              <div className="progress">
+              <div className="progress-bar progress-bar100" role="progressbar"  aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+            </a>
+            <a href="#" className="list-group-item list-group-item-action">
+              <p className="mb-1">Java:</p>
+              
+              <div className="progress">
+              <div className="progress-bar progress-bar100" role="progressbar"  aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+            </a>
+            <a href="#" className="list-group-item list-group-item-action">
+              <p className="mb-1">React:</p>
+              
+              <div className="progress">
+              <div className="progress-bar progress-bar75" role="progressbar"  aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+            </a>
+            <a href="#" className="list-group-item list-group-item-action">
+              <p className="mb-1">Javascript:</p>
+              
+              <div className="progress">
+              <div className="progress-bar progress-bar85" role="progressbar"  aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+            </a>
+            <a href="#" className="list-group-item list-group-item-action">
+              <p className="mb-1">Flutter:</p>
+              
+              <div className="progress">
+              <div className="progress-bar  progress-bar50" role="progressbar"  aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+            </a>
+            <a href="#" className="list-group-item list-group-item-action">
+              <p className="mb-1">C++:</p>
+              
+              <div className="progress">
+              <div className="progress-bar progress-bar75" role="progressbar"  aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+            </a>
+            <a href="#" className="list-group-item list-group-item-action">
+              <p className="mb-1">C#:</p>
+              
+              <div className="progress">
+              <div className="progress-bar progress-bar70" role="progressbar"  aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+            </a>
+            <a href="#" className="list-group-item list-group-item-action">
+              <p className="mb-1">Kotlin:</p>
+              
+              <div className="progress">
+              <div className="progress-bar progress-bar85" role="progressbar"  aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+            </a>
+            <a href="#" className="list-group-item list-group-item-action">
+              <p className="mb-1">Express:</p>
+              
+              <div className="progress">
+              <div className="progress-bar progress-bar80" role="progressbar"  aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+            </a>
+            <a href="#" className="list-group-item list-group-item-action">
+              <p className="mb-1">SQL:</p>
+              
+              <div className="progress">
+              <div className="progress-bar progress-bar100" role="progressbar"  aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
             </a>
           </div>
         </div>
